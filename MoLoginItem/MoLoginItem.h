27ba6da8cddd449c4bf21cd7384eb21af26ec3a3
Sanjay Madan <sanjayma@gmail.com>
2016-12-09 11:08:44 -0800

    Initial Commit

---
+++ MoLoginItem/MoLoginItem.h
@@ -0,0 +1,19 @@
+//
+//  MoLoginItem.h
+//  MoLoginItem
+//
+//  Created by Sanjay Madan on 12/9/16.
+//  Copyright © 2016 Mowglii. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MoLoginItem.
+FOUNDATION_EXPORT double MoLoginItemVersionNumber;
+
+//! Project version string for MoLoginItem.
+FOUNDATION_EXPORT const unsigned char MoLoginItemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoLoginItem/PublicHeader.h>
+
+
